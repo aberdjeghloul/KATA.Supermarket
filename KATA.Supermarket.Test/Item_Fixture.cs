e9d5c218793c85cf74f2226f1a947e9b28556ff7
Abdelrhamane BERDJEGHLOUL <Abdelrhamane.BERDJEGHLOUL@xandrie.com>
2022-02-28 11:54:18 +0100

    KATA.Supermarket.Test: Writing test case for checking if each item has a unique item number

--- KATA.Supermarket.Test/Item_Fixture.cs
+++ KATA.Supermarket.Test/Item_Fixture.cs
@@ -11,5 +11,13 @@ namespace KATA.Supermarket.Test
             Item item = new Item("can of beans");
             Assert.AreEqual<string>(item.GetName(), "can of beans");
         }
+
+        [TestMethod]
+        public void Items_should_have_a_unique_itemnumber()
+        {
+            Item item1 = new Item("can of beans");
+            Item item2 = new Item("can of beans");
+            Assert.IsFalse(item1.GetItemNumber() == item2.GetItemNumber());
+        }
     }
 }
